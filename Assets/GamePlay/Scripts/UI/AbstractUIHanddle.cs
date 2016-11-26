75fdceefbf8b12ecc4117a77a3edf34c0d1f38d9
MizzKii <MizzKiiPC@gmail.com>
2016-11-26 16:27:18 +0700

    Rector code and scene gameplay agian because i fotgot add something in git

---
+++ Assets/GamePlay/Scripts/UI/AbstractUIHanddle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class AbstractUIHanddle : AbstractGamePlay {
+
+	protected GameObject getUIStartObject () {
+		return getGamePlay ().getInstance ().uiStart;
+	}
+
+	protected GameObject getUIPlayObject () {
+		return getGamePlay ().getInstance ().uiPlay;
+	}
+
+	protected GameObject getUIEndObject () {
+		return getGamePlay ().getInstance ().uiEnd;
+	}
+
+	protected HeroController getHeroController () {
+		return getGamePlay ().getInstance ().getHeroController ();
+	}
+}
---
+++ Assets/GamePlay/Scripts/UI/UIStartHanddle.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIStartHanddle : AbstractUIHanddle {
+
+	public void onPlay () {
+		getHeroController ().setStage (HeroStage.run);
+		getUIStartObject ().SetActive (false);
+		getUIPlayObject ().SetActive (true);
+	}
+
+	public void onSetting () {
+
+	}
+
+	public void onHelp () {
+
+	}
+
+	public void onGift () {
+
+	}
+}
